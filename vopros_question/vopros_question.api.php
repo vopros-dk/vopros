23138b626d1ba86a7adb12330d6576f950e672ab
Arne Jørgensen <arne@arnested.dk>
2014-06-20 08:13:05 +0200

    Added API documentation.

---
+++ vopros_question/vopros_question.api.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * @file
+ * API documentation for Vopros Question module.
+ */
+
+/**
+ * Implements hook_vopros_question_user_answer_preference().
+ */
+function hook_vopros_question_user_answer_preference() {
+
+}
+
+/**
+ * Alter Question extras fields.
+ */
+function hook_vopros_question_field_extra_fields_alter(&$extra) {
+
+}
+
+/**
+ * Alter Question status reasons.
+ */
+function hook_vopros_question_status_reasons_alter(&$reasons) {
+
+}
+
+/**
+ * Alter Quanda service options.
+ */
+function hook_vopros_question_qanda_serice_options_alter(&$options) {
+
+}
+
+/**
+ * Alter Question status reason options.
+ */
+function hook_vopros_question_status_reason_options_alter(&$type, &$options) {
+
+}
+
+/**
+ * Alter Question view.
+ */
+function hook_vopros_question_view_alter(&$build, &$type) {
+
+}
