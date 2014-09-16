4693b7ef9b20915c70d01f29f6469b1e3755aa03
Thomas Fini Hansen <xen@xen.dk>
2014-09-16 20:40:40 +0200

    Add answer preference selector. Closes VOP-567.

--- vopros_question/js/vopros_question.base.js
+++ vopros_question/js/vopros_question.base.js
@@ -10,7 +10,7 @@
   Drupal.behaviors.voprosQuestionList = {
     attach: function(context, settings) {
       $("#views-exposed-form-vopros-question-list-page input.form-submit").hide();
-      $("#views-exposed-form-vopros-question-list-page select").change(function() {
+      $("#views-exposed-form-vopros-question-list-page select, #views-exposed-form-vopros-question-list-page input").change(function() {
         $("#views-exposed-form-vopros-question-list-page input.form-submit").click();
         // We need to wait a little bit for IE to send the request and send
         // the request before disabling the dropdowns.
