1210e358608b8d7fa2bfdec0d3ad61df696d6082
Thomas Fini Hansen <xen@xen.dk>
2016-06-23 20:51:38 +0200

    Improve default tab styling

--- modules/vopros/vopros_embed/vopros-embed-tab.tpl.php
+++ modules/vopros/vopros_embed/vopros-embed-tab.tpl.php
@@ -8,5 +8,5 @@
 <div>
   <span class="ask-vopros-icon">?</span>
   <span class="ask-vopros-text"><a href='<?php print $url; ?>'><?php print $title; ?></a></span>
-  <span class="ask-vopros-minimize">&gt;</span>
+  <span class="ask-vopros-minimize"><?php print t('(hide)'); ?></span>
 </div>
